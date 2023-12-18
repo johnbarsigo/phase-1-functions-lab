835fee60a83e815489ffcbc9f33fca4e063f70ff
johnbarsigo <john.barsigo@student.moringaschool.com>
2023-12-18 19:21:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks ( block ){
+    let blocks =  block - 42;
+    if (blocks < 0){
+        blocks = blocks * -1
+    }
+    return blocks
+}
+
+function distanceFromHqInFeet ( block ){
+    let blocks = distanceFromHqInBlocks( block )
+    let feetBlock = 264
+    let distanceInFeet = blocks * feetBlock
+    return distanceInFeet
+}
+
+function distanceTravelledInFeet ( startBlock, endBlock ){
+    let blocksDifference = startBlock - endBlock
+    if (blocksDifference < 0){
+        blocksDifference = blocksDifference * -1
+    }
+    let distanceTravelledFeet = blocksDifference * 264
+    return distanceTravelledFeet
+}
+
+function calculatesFarePrice ( startBlock, endBlock ){
+    let distanceTravelledFeet = distanceTravelledInFeet ( startBlock, endBlock )
+
+    let fare
+    let pricePerFoot = 0.02
+    if (distanceTravelledFeet <= 400 ){
+        fare = 0
+        return fare
+    }
+    else if ( distanceTravelledFeet > 400 && distanceTravelledFeet <= 2000 ){
+        fare = ( distanceTravelledFeet - 400 )* pricePerFoot
+        return fare
+    }
+    else if ( distanceTravelledFeet >2000 && distanceTravelledFeet <= 2500 ){
+        fare = 25
+        return fare
+    }
+    else if ( distanceTravelledFeet > 2500 ){
+        return 'cannot travel that far'
+    }
+}
